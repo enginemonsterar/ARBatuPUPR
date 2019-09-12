2d62ff84c0dd0f70f5a9e3ba7a215caaa0cdce0c
Januar Elsan <januarelsan@gmail.com>
2019-09-12 13:39:43 +0700

    Upload Project

---
+++ Assets/_Main/Scripts/TutorialController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TutorialController : MonoBehaviour {
+
+	private int pageIndex;
+	[SerializeField] private GameObject box;
+	[SerializeField] private GameObject pageIndicator;
+	[SerializeField] private GameObject getStartedButtonObject;
+
+	[Header("UI")]
+	[SerializeField] private Sprite[] pageIndicatorSprites;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void Next(){
+		if(pageIndex < 2){
+			box.transform.GetChild(pageIndex).gameObject.SetActive(false);
+			pageIndicator.transform.GetChild(pageIndex).GetComponent<Image>().sprite = pageIndicatorSprites[0];
+			pageIndex++;
+			box.transform.GetChild(pageIndex).gameObject.SetActive(true);
+			pageIndicator.transform.GetChild(pageIndex).GetComponent<Image>().sprite = pageIndicatorSprites[1];
+		}
+
+		if(pageIndex == 2){
+			getStartedButtonObject.SetActive(true);
+		}
+	}
+
+	public void Prev(){
+		if(pageIndex > 0){
+			box.transform.GetChild(pageIndex).gameObject.SetActive(false);
+			pageIndicator.transform.GetChild(pageIndex).GetComponent<Image>().sprite = pageIndicatorSprites[0];
+			pageIndex--;
+			box.transform.GetChild(pageIndex).gameObject.SetActive(true);
+			pageIndicator.transform.GetChild(pageIndex).GetComponent<Image>().sprite = pageIndicatorSprites[1];
+
+			//Deactive getStartedButtonObject
+			getStartedButtonObject.SetActive(false);
+		}
+	}
+}
